9a6b21dda66c79ba865037e8ac1d261c0ea9b352
jacintogomez <63368475+jacintogomez@users.noreply.github.com>
2022-12-07 14:37:56 -0500

    adding basic pages

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name="home_page"),
+    path('', views.home_page, name='home_page'),
+    path('education/',views.education_page,name='education'),
+    path('workexperience/',views.work_experience_page,name='work experience'),
+    path('projects/',views.projects_page,name='projects')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def home_page(request):
     return render(request,"home.html",{})
+
+def education_page(request):
+    return render(request,"education.html",{})
+def work_experience_page(request):
+    return render(request,"workexperience.html",{})
+def projects_page(request):
+    return render(request,"projects.html",{})
